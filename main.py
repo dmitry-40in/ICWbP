ebe5361204daf76fb2d5c4b5536f26b3fefb77f3
Dmitry <dmitry_40in@mail.ru>
2023-04-30 00:36:18 +0300

    add remove note by id

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import datetime
-
 from datetime import datetime
 
 print('Приложение заметки v.1.0')
@@ -22,17 +20,9 @@ while True:
             try:
                 with open('db.csv', 'r', encoding='utf8') as data:
                     i = data.readlines()
-                    # print('i= ', i)
-                    # print('len_i= ', len(i))
-
                     last_line = i[len(i) - 1].split(';')
-                    # print('last_line= ', last_line)
-
                     last_id = int(last_line[0])
-                    # print('last_id= ', last_id)
-
                     id = str(last_id + 1)
-                    # print('id= ', id)
             except FileNotFoundError:
                 id = '1'
 
@@ -66,11 +56,9 @@ while True:
                 for i in data.readlines():
                     line_i = i.split(';')
                     if note_number == line_i[0]:
-                    #     print(i[0])
                         note_edit = line_i.copy()
                         print(f'Редактируем заметку ID = {note_number}')
                         while flag:
-
                             choice_edit = input('Выберите действие:\n1 - изменить заголовок заметки\n2 - изменить тело заметки\n3 - сохранить и выйти в основное меню\n')
                             match choice_edit:
                                 case '1':
@@ -80,7 +68,6 @@ while True:
                                 case '3':
                                     dt = datetime.now()
                                     note_edit[3] = "{}.{}.{} - {}:{}\n".format(dt.day, dt.month, dt.year, dt.hour, dt.minute)
-                                    print('Досвидания.')
                                     list_edit.append(';'.join(note_edit))
                                     flag = False
                                 case _:
@@ -89,17 +76,28 @@ while True:
                     else:
                         
                         list_edit.append(i)
-                        print('type list_edit= ', type(list_edit), ': ', list_edit)
             with open('db.csv', 'w', encoding='utf8') as data:
                 for i in list_edit:
-                    print('type_i= ', type(i), ' ', i)
                     data.write(i)
 
             
 
         case '5':
             print('Вы выбрали 5')
+            list_edit = []
+            note_edit = []
+            note_number = input('Введите ID заметки для удаления: ')
+            flag = True
+            with open('db.csv', 'r', encoding='utf8') as data:
+                for i in data.readlines():
+                    line_i = i.split(';')
+                    if note_number != line_i[0]:
+                        list_edit.append(i)
 
+            with open('db.csv', 'w', encoding='utf8') as data:
+                for i in list_edit:
+                    data.write(i)
+            print(f'Заметка с ID = {note_number} удалена.')
         case '6':
             print('Досвидания.')
             break
